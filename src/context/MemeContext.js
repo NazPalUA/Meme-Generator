a6792cc76144bc536acab38d90c8052e864d5fec
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2022-10-14 23:14:43 +0300

    moved linesArr from MemeContext to Meme

--- src/context/MemeContext.js
+++ src/context/MemeContext.js
@@ -7,7 +7,7 @@ const MemeContext = React.createContext()
 function MemeContextProvider({children}) {
     const memeRef = useRef()
 
-    const [dragStart, setDragStart] = useState({x: 0, y: 0})
+
 
     const [meme, setMeme] = useState({
         linesArr: [
@@ -98,74 +98,19 @@ function MemeContextProvider({children}) {
         }))
     }
 
-    function linesArr() {
-        const style = linesStyle(meme)
-        return meme.linesArr.map(line => {
-            const positionStyle = {
-                top: `${line.top}px`,
-                left: `${line.left}px`,
-            }
-            return line.active ?
-            <input autoFocus
-                key={line.lineId}
-                id={line.lineId}
-                type="text"
-                placeholder="Type here"
-                className={`meme__text active`}
-                name="topText"
-                value={line.text}
-                onChange={handleInputChange}
-                style={{...style, ...positionStyle}}
-                onFocus={(event) => event.target.select()}
-                onBlur={() => removeActive(line.lineId)}
-                size={line.text.length}
-            /> :
-            <div
-                draggable
-                onDragStart={handleDragStart}
-                onDragOver={handleDragOver}
-                onDragEnd={handleDragEnd}
 
-                key={line.lineId}
-                id={line.lineId}
-                className={`meme__text no-select`}
-                onDoubleClick={(event) => handleDoubleInputClick(event, line.lineId)}
-                style={{...style, ...positionStyle}}
-            >
-                {line.text}
-            </div>
-        })
-    }
-
-    function handleDragStart(event) {
-        event.target.classList.add("dragging")
-        setDragStart({x: event.clientX, y: event.clientY})
-    }
-
-    function handleDragOver(event) {
-        event.preventDefault()
-    }
-
-    function handleDragEnd(event) {
-        event.target.classList.remove("dragging")
-        const deltaX = event.clientX - dragStart.x
-        const deltaY = event.clientY - dragStart.y
-
+    function moveLine(activeLineId, deltaX, deltaY) {
         setMeme(prevMeme => ({
             ...prevMeme,
-            lastActiveLineId: event.target.id,
+            lastActiveLineId: activeLineId,
             linesArr: prevMeme.linesArr.map(line => {
-                if(event.target.id !== line.lineId) return line
+                if(activeLineId !== line.lineId) return line
                 else return {...line, 
                     top: line.top + deltaY,
                     left: line.left + deltaX
                 }
             })
         }))
-    }
-
-    function handleDoubleInputClick(event, lineId) {
-        setActive(lineId)
     }
 
     function handleInputChange(event) {
@@ -231,13 +176,16 @@ function MemeContextProvider({children}) {
             allMemes,
             addLine,
             setImg,
-            linesArr,
             toggleUpperCase,
             changeTextSize,
             changeShadowSize,
             handleSettingsChange,
             centerText,
-            addToAllMemes
+            addToAllMemes,
+            removeActive,
+            handleInputChange,
+            setActive,
+            moveLine
         }}>
             {children}
         </MemeContext.Provider>
