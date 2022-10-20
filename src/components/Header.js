17420a961aaa29b0e3bb15cbdaef3f681f319bb4
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2022-10-20 16:57:27 +0300

    header and container styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,22 +7,24 @@ import bookmark from "../images/bookmark.png"
 export default function Header() {
     return (
         <header className="header">
-            <Link to="/" className="header__logo-link">
-                <img 
-                    src={trollFace} 
-                    className="header__logo"
-                    alt="troll face"
-                />      
-                <h2 className="header__title">Meme Generator</h2>
-            </Link>
-            <Link to="/saved" className="header__saved-link">
-                <span className="header__saved-num">0</span>
-                <img 
-                    src={bookmark} 
-                    className="header__saved-img"
-                    alt="bookmark pic"
-                />   
-            </Link>
+            <div className="container header__container">
+                <Link to="/" className="header__main-link">
+                    <img 
+                        src={trollFace} 
+                        className="header__logo"
+                        alt="troll face"
+                    />      
+                    <h1 className="header__title">Meme Generator</h1>
+                </Link>
+                <Link to="/saved" className="header__saved-link">
+                    <span className="header__saved-num">0</span>
+                    <img 
+                        src={bookmark} 
+                        className="header__saved-img"
+                        alt="bookmark pic"
+                    />   
+                </Link>
+            </div>
         </header>
     )
 }
