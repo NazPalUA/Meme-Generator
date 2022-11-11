c19ebbf108e6e9c4eaac6894f8d01332d6053dbe
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2022-11-11 04:56:44 +0200

    auto sizing with css only

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,14 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import useWindowSize from "../tools/useWindowSize"
 import HorizontalScroll from 'react-horizontal-scrolling'
-import { MemeContext } from "../context/MemeContext"
+
 import ImagesSidebar from  "../components/ImagesSidebar"
 import MainSection from "../components/MainSection"
 import SettingsSidebar from "../components/SettingsSidebar"
 import "../css/home.css"
 
 export default function Home() {
-    const {meme} = useContext(MemeContext)
     const size = useWindowSize()
-
-    const [changeSizeTrigger, setChangeSizeTrigger] = useState(1)
-    useEffect(() => {
-        setTimeout(()=>setChangeSizeTrigger(2), 100)
-    }, [])
-
-    const [imagesSidebarStyle, setImagesSidebarStyle] = useState()
 
     const [isThreeColumnLayout, setIsThreeColumnLayout] = useState(size.width >= 768)
 
@@ -24,37 +16,24 @@ export default function Home() {
         setIsThreeColumnLayout(size.width >= 768)
     }, [size])
 
-    function getImagesSidebarStyle() {
-        const viewportHeight = size.height ? size.height : window.innerHeight
-        const headerHeight = document.getElementById("header").offsetHeight
-        const mainHeight = document.getElementById("meme-container").offsetHeight
 
-        if(isThreeColumnLayout) return {
-            minHeight: viewportHeight-headerHeight,
-            height: mainHeight
-        }
-    }
-
-    useEffect(()=>{
-        setImagesSidebarStyle(getImagesSidebarStyle)
-    }, [meme.img, size, changeSizeTrigger])
 
     return (
         <main className="home-page">
             <div className="container main__container" id="main__container">
                 {!isThreeColumnLayout ? 
-                    <HorizontalScroll className="section section_images-sidebar" style={imagesSidebarStyle}>
+                    <HorizontalScroll className="section section_images-sidebar">
                         <ImagesSidebar />
                     </HorizontalScroll>
                 :
-                    <section className="section section_images-sidebar" style={imagesSidebarStyle}>
+                    <section className="section section_images-sidebar">
                         <ImagesSidebar />
                     </section>
                 }
                 <section className="section section_main">
                     <MainSection />
                 </section>
-                <section className="section section_settings-sidebar"  style={imagesSidebarStyle}>
+                <section className="section section_settings-sidebar">
                     <SettingsSidebar />
                 </section>
             </div>
