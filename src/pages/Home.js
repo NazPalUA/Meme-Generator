679ec65bb4f1d486b68072df9b457a11feefa1b1
NazPalUA <104204125+NazPalUA@users.noreply.github.com>
2022-12-23 23:37:54 +0200

    Home .scss/.js refactoring

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,14 @@ import "./Home.scss"
 export default function Home() {
     return (
         <main className="home-page">
-            <div className="container main__container" id="main__container">
-                <section className="section section_images-sidebar">
+            <div className="container home-page__container" id="main__container">
+                <section className="home-page__section home-page__section_images-sidebar">
                     <ImagesSidebar />
                 </section>
-                <section className="section section_main">
+                <section className="home-page__section home-page__section_main">
                     <MainSection />
                 </section>
-                <section className="section section_settings-sidebar">
+                <section className="home-page__section home-page__section_settings-sidebar">
                     <SettingsSidebar />
                 </section>
             </div>
